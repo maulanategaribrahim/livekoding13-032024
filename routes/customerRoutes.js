5fa3c1810e493f9addb5efbe8f35d74393126fcc
Maulana Tegar I <tegarmaulana973@gmail.com>
2024-03-15 23:09:37 +0700

    KODING 1503

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const customerController = require('./controllers/customersController')
+const customerController = require('../controllers/customerController')
 
 router
     .route("/")
